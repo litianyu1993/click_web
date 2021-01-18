10a2a1c799d4e68ce0c9485979ad1e4379243724
litianyu1993 <tianyu.li@mail.mcgill.ca>
2021-01-18 11:41:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from flask import jsonify, Response
+import numpy as np
+import json
 
-from flask import Flask
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+#
+# app = Flask(__name__)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*.officeapps.live.com"}})
 
-@app.route("/")
-def index():
-    return "<h1>Hello Azure!</h1>"
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return json.JSONEncoder.default(self, obj)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+es = []
+
+@app.route('/', methods=['POST', 'GET'])
+@cross_origin()
+def login():
+    if request.method == 'POST':
+        es.append(request.data)
+
+        return 'ok'
+    else:
+        json_es = json.dumps(es, cls=MyEncoder,indent=4)
+        return json_es
+
+if __name__ == '__main__':
+    app.run()
